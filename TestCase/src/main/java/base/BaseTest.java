56cd0caa567379f527b3d1182b99e9af65f889d6
Thanhvt1007 <Vothithanh921819@gmail.com>
2024-12-07 21:16:39 +0700

    login test

--- TestCase/src/main/java/base/BaseTest.java
+++ TestCase/src/main/java/base/BaseTest.java
@@ -1,25 +1,19 @@
 package base;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import untils.ReadConfig;
 
 public class BaseTest {
-   public static WebDriver driver;
+
 
     @BeforeMethod
     public void setUp() {
-//        BaseSetup.setupDriver();
-        driver = new ChromeDriver();
-        driver.get("http://localhost:8080/yourstyle/accounts/login");
-        driver.manage().window().maximize();
+        BaseSetup.setupDriver();
     }
 
     @AfterMethod
     public void tearDown() {
-       driver.quit();
+       BaseSetup.driver.quit();
     }
 
 }
